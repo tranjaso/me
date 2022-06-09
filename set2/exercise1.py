9321d7b8b2917a7c24d066ba90cf9d6e427c30e1
Jason Tran <tranjason@hotmail.com>
2022-06-09 17:00:50 +1000

    Comments added

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -15,27 +15,32 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
+# I think this creates a list, and nests the words into a collection which is declared in some_words
 some_words = ["what", "does", "this", "line", "do", "?"]
 
+# I think this creates a loop for the words in the list which can all be called in the print function
 for word in some_words:
-    print(word)
+    print(word) # it prints "what, does, this, line, do, ?" 
 
+# I think this is repeition, similiar to above, the only difference is that it is just declared as x now 
 for x in some_words:
     print(x)
 
-print(some_words)
+print(some_words) # this will print out everything which is ["what", "does", "this", "line", "do", "?"]
 
-if len(some_words) > 3:
-    print("some_words contains more than 3 words")
+# I think that if the list contains more than 3 words it will print the string below if the case was unsucessful it will
+# do nothing
+if len(some_words) > 3: 
+    print("some_words contains more than 3 words") # in this case it will print what was written 
 
-
-def usefulFunction():
+# defines a function as usefulFunction
+def usefulFunction(): 
     """
     You may want to look up what uname does before you guess
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    print(platform.uname()) # prints out the user's specification about their system
 
 
-usefulFunction()
+usefulFunction() # calls in the function, which allows the code above to run.
